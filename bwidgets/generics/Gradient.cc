273757b2f170eeb6993d1a28ab45039784fa0b98
Julian Eisel <eiseljulian@gmail.com>
2017-08-09 16:08:29 +0200

    Delete redundant file

--- bwidgets/generics/Gradient.cc
+++ bwidgets/generics/Gradient.cc
@@ -1,4 +1,5 @@
 #include <cassert>
+
 #include "Point.h"
 
 #include "Gradient.h"
--- bwidgets/styles/BlenderClassic.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace bWidgets {
-
-struct StyleBlenderClassic {
-	
-};
-
-} // namespace bWidgets
