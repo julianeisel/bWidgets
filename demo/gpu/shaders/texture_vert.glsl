e6048d228657178b8d8f926d267d05bd365d15ad
Julian Eisel <eiseljulian@gmail.com>
2018-04-07 17:57:26 +0200

    Change license of Demo application to GPL 2 or later
    Reason for this is mainly to avoid further licensing issues when adding
    components based on Blender (e.g. Blender code or icons). With the MIT license,
    we'd have to ask every contributor of GPL components for their permission to use
    their contributions. With the demo app being GPL2+ we can use their
    contributions without permission.
    Also, while I don't think GPL makes sense for bWidgets itself, it's fine for the
    demo app. I support the philosophy behind the GPL, so I like to use it where
    reasonable.
    
    It seems generally suggested to make copyright clear on a per file basis - at
    least for the original code. Thus I decided to add copyright notices to all
    files. Some code is based on Blender code, so I tried to find out the copyright
    holders of it and added them to the copyright notice. I can not guarantee that
    these copyright notices are correct, but as I understand they are only acting as
    a good hint anyway.

--- demo/gpu/shaders/texture_vert.glsl
+++ demo/gpu/shaders/texture_vert.glsl
@@ -1,3 +1,24 @@
+/*
+ * ***** BEGIN GPL LICENSE BLOCK *****
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Original work Copyright (c) 2016, 2018 Julian Eisel, Martijn Berger
+ *
+ * ***** END GPL LICENSE BLOCK *****
+ */
+
 #version 330 core
 
 uniform mat4 ModelViewProjectionMatrix;
@@ -11,6 +32,5 @@ noperspective out vec2 texCoord_interp;
 void main()
 {
 	gl_Position = ModelViewProjectionMatrix * vec4(pos, 0.0, 1.0);
-
 	texCoord_interp = texCoord;
 }
