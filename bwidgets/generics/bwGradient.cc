17105dda5761f007fb2b012dd89942991bdfe853
Julian Eisel <eiseljulian@gmail.com>
2018-10-13 23:46:28 +0200

    Fix failing assert when changing interface scale with gradients drawn

--- bwidgets/generics/bwGradient.cc
+++ bwidgets/generics/bwGradient.cc
@@ -29,7 +29,7 @@ bwColor bwGradient::calcPointColor(
 	                          ((bounding_box.xmax - point.x) / (float)bounding_box.width());
 	const float pos_fac_inv = 1.0f - pos_fac;
 
-	assert((pos_fac > -1e-6f) && (pos_fac <= 1.0f));
+	assert((pos_fac > -1e-5f) && ((pos_fac - 1.0f) <= 1e-6f));
 
 	return bwColor((begin[0] * pos_fac) + (end[0] * pos_fac_inv),
 	               (begin[1] * pos_fac) + (end[1] * pos_fac_inv),
